d0e54dc744f13c7fd4b7fa14a1ebf387e0a391fb
Anna-kay-Joiles <150190817+Anna-kay-Joiles@users.noreply.github.com>
2024-11-02 13:03:47 -0600

    student portal

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () => {
     <nav className="bg-gray-800 w-full fixed top-0 left-0 z-10">
     <div className="max-w-screen-xl mx-auto p-4">
       <h1 className="text-white text-2xl font-bold">New Generation High School</h1>
+      <h2 className="text-white text-2xl semi-bold"> Student Portal</h2>
     </div>
   </nav>
   );
