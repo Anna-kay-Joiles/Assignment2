c5d872e070a657a9c2e06d25796acef05217f997
Anna-kay-Joiles <150190817+Anna-kay-Joiles@users.noreply.github.com>
2024-11-02 13:01:58 -0600

    Change the name

--- components/Footer.js
+++ components/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
 <footer className="bg-gray-300 p-4 text-center fixed bottom-0 left-0 w-full z-10">
 <p className="font-sans font-semibold">
-        THE DEAA Institute <br />
+        New Generation High School <br />
         Address: 2454 13 Northridge West South East, T2E8HM. <br />
         Contact Number: (785) 782-5781 <br />
         Fax Number: (784) 552 5455 <br />
         Email: 
         <a href="#" className="hover:underline hover:decoration-red-600">
-          DEAA@INSTITUTE.ORG
+          Students@newgeneration.ORG
         </a>
       </p>
     </footer>
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 w-full fixed top-0 left-0 z-10">
     <div className="max-w-screen-xl mx-auto p-4">
-      <h1 className="text-white text-2xl font-bold">THE DEAA Institute</h1>
+      <h1 className="text-white text-2xl font-bold">New Generation High School</h1>
     </div>
   </nav>
   );
